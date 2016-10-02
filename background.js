a6f1fd9b324ee7286544eb40a6ae449ee1bb9d67
broad-well <michaelpeng2002@icloud.com>
2016-10-01 23:41:37 -0700

    styling/background.js

--- background.js
+++ background.js
@@ -1,25 +1,108 @@
+
+document.KentClasses = {
+	"5th Grade Band": "BAND5",
+	"6th Grade Band": "BAND6",
+	"6th Grade Chorus": "CHOR6",
+	"Academic Workshop": "AWKS",
+	"Advanced Band": "BANDA",
+	"Advanced Choir": "CHORA",
+	"Algebra 8": "ALGE8",
+	"All Classes": "",
+	"Architecture 8": "ARCH8",
+	"Art 5, Art 6, Art 7, Art 8": "ART",
+	"Coding 7": "CODE7",
+	"Commencement Band": "BANDC",
+	"Core 6 - Karlin": "CORE6-KAR",
+	"Core 6 - Ms. Bridges": "CORE6-BRI",
+	"Core 6 - Wells": "CORE6-WEL",
+	"Drama 7": "DRAM7",
+	"ELA/History Core 5 - Torrano": "ELAH5-TOR",
+	"ELA/History Core 5 - Hagan": "ELAH5-HAG",
+	"ELA/History Core 5 - Jelen": "ELAH5-JEL",
+	"ELA/History Core 5 - Stephens": "ELAH5-STE",
+	"English Language Development": "ELD",
+	"Enterprise 7": "ENTR7",
+	"Expressions 3D 7": "EX3D7",
+	"Expressions 5": "EXPR5",
+	"Expressions 6 - Goetz": "EXPR6-GOE",
+	"Expressions 6 - Karlin": "EXPR6-KAR",
+	"Expressions 6 - Osterman": "EXPR6-OST",
+	"Expressions 6 - Wells": "EXPR6-WEL",
+	"Expressions 8": "EXPR8",
+	"Extended Math": "MATHX",
+	"Extended Reading": "READX",
+	"Film Making": "FILM78",
+	"History 6 - Goetz": "HIST6-GOE",
+	"History 7 - Mr. Palmer": "HIST7-PAL",
+	"History 7 - Ms. Ward": "HIST7-WAR",
+	"History 8 - Mr. Palmer": "HIST8-PAL",
+	"History 8 - Ms. Ward": "HIST8-WAR",
+	"Jazz Big Band": "BANDB",
+	"Jazz Combo": "JAZZC",
+	"Journalism 8": "JOUR8",
+	"Kent Learning Center": "LRNC",
+	"Kent Makers 6": "MAKR6",
+	"Kent Makers 8": "MAKR8",
+	"Language Arts 6 - Goetz": "ELA6-GOE",
+	"Language Arts 7 - Maltzman": "ELA7-MAL",
+	"Language Arts 7 - Moretti": "ELA7-MOR",
+	"Language Arts 8 - Anderson": "ELA8-AND",
+	"Language Arts 8 - Gavin": "ELA8-GAV",
+	"Life Skills": "LIFE",
+	"Math 7+ - Lentini": "PALG7",
+	"Math/Science Core 5 - Bridgman": "MSCI5-BRI",
+	"Math/Science Core 5 - Chusid": "MSCI5-CHU",
+	"Math/Science Core 5 - Gallagher": "MSCI5-GAL",
+	"Mathematics 6 - Klima": "MATH6-KLI",
+	"Mathematics 6 - Lentini": "MATH6-LEN",
+	"Mathematics 7 - Widelock": "MATH7-WID",
+	"Mathematics 7 - Klima": "MATH7-KLI",
+	"Mathematics 8 - Hettleman": "MATH8-HET",
+	"Mathematics 8 - Widelock": "MATH8-WID",
+	"PE 5": "PE5",
+	"PE 6": "PE6",
+	"PE 7": "PE7",
+	"PE 8": "PE8",
+	"Poetry 8": "POET8",
+	"Robotics 5": "ROBT5",
+	"Rock Band": "ROCKB",
+	"Science 6": "SCI6",
+	"Science 7": "SCI7",
+	"Science 8": "SCI8",
+	"Spanish A": "SPANA",
+	"Spanish B": "SPANB",
+	"Woodworking 5": "WOOD5",
+	"Woodworking 8": "WOOD8"
+};
+
+const OmniBoxSuggestion = "Try returning your class ID. Don't remember? Enter a backslash for a directory.";
+
 function resetDefaultSuggestion() {
-  chrome.omnibox.setDefaultSuggestion({
-      description: "Try returning your class ID. Don't remember? Enter a backslash for a directory."
-  });
+	chrome.omnibox.setDefaultSuggestion({
+		description: OmniBoxSuggestion
+	});
 }
 
 resetDefaultSuggestion();
 
-chrome.omnibox.onInputChanged.addListener(function(text, suggest) {
-"Try returning your class ID. Don't remember? Enter a backslash for a directory."
-});
-
-chrome.omnibox.onInputCancelled.addListener(function() {
-  resetDefaultSuggestion();
-});
+chrome.omnibox.onInputChanged.addListener(resetDefaultSuggestion);
+chrome.omnibox.onInputCancelled.addListener(resetDefaultSuggestion);
 
 function navigate(url) {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.update(tabs[0].id, {url: url});
-  });
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		chrome.tabs.update(tabs[0].id, {url: url});
+	});
+}
+
+function inObject(str, obj) {
+	for (key in obj) {
+		if (obj[key] === str || key === str) return true;
+	}
+	return false;
 }
 
 chrome.omnibox.onInputEntered.addListener(function(text) {
-  navigate("http://www.edlinesites.net/pages/Kent_Middle_School/Classes/" + text);
+	inObject(text.toUpperCase(), document.KentClasses) ? 
+		navigate("http://www.edlinesites.net/pages/Kent_Middle_School/Classes/" + text) :
+		alert(`Class ${text} does not exist. Try again!`);
 });
--- newtab.js
+++ newtab.js
@@ -1,19 +1,3 @@
-//var mv = document.getElementById("most-visited");
-//var xhr = new XMLHttpRequest();
-//xhr.open("get", chrome.extension.getURL("newtab.html"));
-//xhr.onreadystatechange = () => {
-//	if (xhr.readyState == 4 && xhr.status == 200) {
-//		mv.innerHTML = xhr.responseText;
-//	}
-//}
-//xhr.send();
-
-
-//var xml = new XMLHttpRequest();
-//xml.open("GET", chrome.extension.getURL("popup.js"), false);
-//xml.send(null);
-//eval(xml.responseText); delete xml;
-
 document.getElementById("most-visited").innerHTML = "<iframe id='iframe' style='width: 672px; height: 500px; background:rgba(255,255,255,0);' src=\"" + chrome.extension.getURL("newtab.html") + "\" frameborder=\"0\"></iframe>";
 
 window.addEventListener("message", function(origin){
@@ -21,3 +5,4 @@ window.addEventListener("message", function(origin){
 		window.location.href = origin.data['*KsRedirectionHref*'];
 	}
 });
+
