7b44eb62ae00e5e68c0bf14a8b14249a49723dc0
Jordan Mann <jordanmann@Jordans-MacBook-Pro.local>
2018-08-29 16:22:33 -0700

    More work to clean

---
+++ newtab.js
@@ -0,0 +1,15 @@
+try{
+	document.getElementById("most-visited").innerHTML =
+		"<iframe id='iframe' style='width: 672px; height: 500px; background:rgba(255,255,255,0);' src=\"" +
+		chrome.extension.getURL("newtab.html") + "\" frameborder='0'></iframe>";
+}catch(e){
+	console.error('RedSearch module failed to load due to error below');
+	throw e;
+}
+
+window.addEventListener("message", function(origin){
+	if(origin.data && origin.data['*KsRedirectionHref*']){
+		window.location.href = origin.data['*KsRedirectionHref*'];
+	}
+});
+
